538eef706c33ba7615f2dc8769aec526c2300b3a
Amanda Caraça <amandacaraca.profissional@gmail.com>
2022-03-24 18:18:25 -0300

    [Recriando Pipefy] - Estudos

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { DndProvider } from 'react-dnd'
+
+import GlobalStyle from './styles/global'
+import Header from './components/Header'
+import Board from './components/Board'
+
+function App() {
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <Header />
+      <Board />
+      <GlobalStyle />
+    </DndProvider>
+  )
+}
+
+export default App
